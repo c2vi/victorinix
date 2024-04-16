deee0a9cb5cc9302cddcf7b089ffe0a1361fb8aa
Sebastian Moser <sebastian@c2vi.dev>
2024-04-16 23:12:19 +0200

    added my config flake as input

--- flake.nix
+++ flake.nix
@@ -4,10 +4,11 @@
     # nixpkgs 35.11 still contains rust 1.73
 		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-
  		flake-utils.url = "github:numtide/flake-utils";
+
+    c2vi-config.url = "github:c2vi/nixos";
   };
-  outputs = { self, flake-utils, nixpkgs, ... }@inputs: 
+  outputs = { self, flake-utils, nixpkgs, c2vi-config, ... }@inputs: 
   
   ####################################
   # outputs the same for every system
@@ -19,7 +20,7 @@
   # system specific outpugs
   flake-utils.lib.eachDefaultSystem (system: let pkgs = nixpkgs.legacyPackages.${system}; in {
 
-    packages.webfiles = pkgs.callPackage ./webfiles.nix {inherit inputs nixpkgs self; url = "http://c2vi.dev"; };
+    packages.webfiles = pkgs.callPackage ./webfiles.nix {inherit inputs nixpkgs self c2vi-config; url = "http://c2vi.dev"; };
     packages.webrun = pkgs.callPackage ./webrun.nix {inherit inputs nixpkgs self; url = "http://c2vi.dev"; };
       
     devShells.default = pkgs.mkShell {
